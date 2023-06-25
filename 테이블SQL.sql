efe9a4e95bee7209bc767613dac1bb4263f11bc2
송근호 <123912792+Song-GeunHo@users.noreply.github.com>
2023-06-26 08:32:16 +0900

    Add files via upload

--- 테이블SQL.sql
+++ 테이블SQL.sql
@@ -1,11 +1,11 @@
-// 데이터베이스 생성 및 권한 부여
+-- 데이터베이스 생성 및 권한 부여
 CREATE USER momento IDENTIFIED BY momento;
 GRANT resource, CONNECT TO momento;
 
-// 글 번호 증가를 위한 시퀀스 생성
+-- 글 번호 증가를 위한 시퀀스 생성
 CREATE SEQUENCE seq_board;
 
-// 공지사항 테이블 생성
+-- 공지사항 테이블 생성
 CREATE TABLE tbl_board (
   bno					NUMBER(10,0),
   title					VARCHAR2(200)				NOT NULL,
@@ -17,12 +17,12 @@ CREATE TABLE tbl_board (
   fixed				CHAR(1)								DEFAULT	'0'
 );
 
-// 기본키 지정
+-- 기본키 지정
 ALTER TABLE tbl_board ADD CONSTRAINT pk_board PRIMARY KEY (bno);
 
 SELECT * FROM tbl_board;
 
-// 공지사항 백업 테이블 생성
+-- 공지사항 백업 테이블 생성
 CREATE TABLE tbl_backup_board(
   bno					NUMBER(10,0),
   title					VARCHAR2(200)				NOT NULL,
@@ -30,16 +30,16 @@ CREATE TABLE tbl_backup_board(
   regdate			DATE,
   updatedate	DATE,
   referrals			NUMBER(10,0)					DEFAULT 0,
-  views				NUMBER(10,0)					DEFAULT 0
+  views				NUMBER(10,0)					DEFAULT 0,
+  deleteddate		DATE					DEFAULT SYSDATE
 );
 
-// 기본키, 외래키 생성
-ALTER TABLE tbl_backup_board MODIFY regdate DEFAULT null;
+-- 기본키 생성
 ALTER TABLE tbl_backup_board ADD CONSTRAINT pk_backup_board PRIMARY KEY (bno);
 
 SELECT * FROM tbl_backup_board;
 
-// 공지사항 글 삭제 시 자동 백업 트리거 생성 
+-- 공지사항 글 삭제 시 자동 백업 트리거 생성 
 CREATE TRIGGER trg_backupBoard
 	AFTER DELETE
 	ON tbl_board
@@ -51,7 +51,7 @@ BEGIN
 		VALUES (:OLD.bno, :OLD.title, :OLD.content, :OLD.regdate, :OLD.updatedate, :OLD.referrals, :OLD.views);
 END;
 
-// 파일 업로드 테이블 생성
+-- 파일 업로드 테이블 생성
 CREATE TABLE tbl_attach (
 		uuid								varchar2(100)	,
 		uploadPath					varchar2(200)	NOT NULL,
@@ -60,6 +60,6 @@ CREATE TABLE tbl_attach (
 		bno									number(10, 0)
 );
 
-// 기본키 외래키 생성
+-- 기본키 외래키 생성
 ALTER TABLE tbl_attach ADD CONSTRAINT pk_attach PRIMARY KEY (uuid);
 ALTER TABLE tbl_attach ADD CONSTRAINT fk_board_attach FOREIGN KEY (bno) REFERENCES tbl_board(bno);
